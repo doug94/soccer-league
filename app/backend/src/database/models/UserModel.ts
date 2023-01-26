4ccbf8d6ff723c82c567289f1e1df229f93610b8
Douglas Alves <douglas_dca@hotmail.com>
2023-01-26 16:17:53 -0300

    Requisito 1 implementado

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,38 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: STRING,
+  },
+  role: {
+    type: STRING,
+  },
+  email: {
+    type: STRING,
+  },
+  password: {
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
