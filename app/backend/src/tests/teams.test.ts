af0d9c0cd11c35bc65803c9b49c562036256653a
Douglas Alves <douglas_dca@hotmail.com>
2023-02-01 16:04:46 -0300

    Testes da rota de times concluído

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -5,6 +5,7 @@ import * as jwt from 'jsonwebtoken';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
+import Teams from '../database/models/TeamsModel';
 
 import { Response } from 'superagent';
 
@@ -16,9 +17,20 @@ const { expect } = chai;
 
 describe('Teste de integração da rota de times', function () {
   let chaiHttpResponse: Response;
+
   it('Deve retornar todos os times corretamente', async function () {
+    sinon.stub(Teams, 'findAll').resolves(mockTeams as any);
     chaiHttpResponse = await chai.request(app).get('/teams');
     expect(chaiHttpResponse.status).to.be.equal(200);
     expect(chaiHttpResponse.body).to.be.deep.equal(mockTeams);
+    sinon.restore();
+  })
+
+  it('Deve retornar o time correspondente com o id no parâmetro da URL', async function () {
+    sinon.stub(Teams, 'findByPk').resolves(mockTeams[1] as any);
+    chaiHttpResponse = await chai.request(app).get('/teams/2');
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(mockTeams[1]);
+    sinon.restore();
   })
 })
