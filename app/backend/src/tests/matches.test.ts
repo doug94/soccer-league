f87f2f724a21c4fe3cd07bb098eb370f296736c4
Douglas Alves <douglas_dca@hotmail.com>
2023-02-03 22:27:00 -0300

    Testes dos requisitos 19 a 21 concluídos

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -4,6 +4,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
+import Matches from '../database/models/MatchesModel';
 
 import { Response } from 'superagent';
 
@@ -14,8 +15,13 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Teste de integração da rota de partidas', function () {
+  afterEach(function () {
+    sinon.restore();
+  })
+
   let chaiHttpResponse: Response;
   it('Deve retornar todas as partidas corretamente', async function () {
+    sinon.stub(Matches, 'findAll').resolves(mockMatches as any);
     chaiHttpResponse = await chai.request(app).get('/matches');
     expect(chaiHttpResponse.status).to.be.equal(200);
     expect(chaiHttpResponse.body).to.be.deep.equal(mockMatches);
@@ -23,15 +29,17 @@ describe('Teste de integração da rota de partidas', function () {
 
   it('Deve retornar todas as partidas em andamento', async function() {
     const ongoingMatches = mockMatches.filter((match) => match.inProgress)
+    sinon.stub(Matches, 'findAll').resolves(ongoingMatches as any);
     chaiHttpResponse = await chai.request(app).get('/matches?inProgress=true');
     expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.equal(ongoingMatches);
+    expect(chaiHttpResponse.body).to.be.deep.equal(ongoingMatches);
   })
 
   it('Deve retornar todas as partiads finalizadas', async function() {
     const finishedMatches = mockMatches.filter((match) => !match.inProgress);
+    sinon.stub(Matches, 'findAll').resolves(finishedMatches as any);
     chaiHttpResponse = await chai.request(app).get('/matches?inProgress=false');
     expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.equal(finishedMatches);
+    expect(chaiHttpResponse.body).to.be.deep.equal(finishedMatches);
   })
 })
