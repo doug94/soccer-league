4e194e7b7548dbc27106478ee58105764e448aac
Douglas Alves <douglas_dca@hotmail.com>
2023-03-20 15:44:04 -0300

    Nome de variável corrigido

--- app/backend/src/controllers/leaderboardController.ts
+++ app/backend/src/controllers/leaderboardController.ts
@@ -2,24 +2,24 @@ import { Request, Response } from 'express';
 import LeaderboardService from '../services/leaderboardService';
 
 class LeaderboardController {
-  public lbController: LeaderboardService;
+  public leaderboardService: LeaderboardService;
 
   public constructor(leaderboardService: LeaderboardService) {
-    this.lbController = leaderboardService;
+    this.leaderboardService = leaderboardService;
   }
 
   public getLeaderboard = async (_req: Request, res: Response) => {
-    const leaderboard = await this.lbController.getStatsFromAllTeams();
+    const leaderboard = await this.leaderboardService.getStatsFromAllTeams();
     return res.status(200).json(leaderboard);
   };
 
   public getLeaderboardHomeTeams = async (_req: Request, res: Response) => {
-    const leaderboard = await this.lbController.getStatsFromAllTeams('home');
+    const leaderboard = await this.leaderboardService.getStatsFromAllTeams('home');
     return res.status(200).json(leaderboard);
   };
 
   public getLeaderboardAwayTeams = async (_req: Request, res: Response) => {
-    const leaderboard = await this.lbController.getStatsFromAllTeams('away');
+    const leaderboard = await this.leaderboardService.getStatsFromAllTeams('away');
     return res.status(200).json(leaderboard);
   };
 }
